a1204ca52d9faa689e0c1e51ef1b80650f0a1480
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-09 18:44:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,9 @@
-def using_push (array, string)
-  
+array_ex = "banana", "apple"
+element_ex = "peach"
+
+
+def using_push (array, element)
+  array << element
+end
+
+using_push (array_ex,element_ex )
